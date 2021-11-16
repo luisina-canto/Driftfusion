0d4b63bd2f677891e92f08a2379ec3f30783f59d
pdb13 <p.calado13@imperial.ac.uk>
2021-11-16 12:45:51 +0000

    changes to allow EF0 and workfunctions to be changed

--- IS_full_method_script.m
+++ IS_full_method_script.m
@@ -30,12 +30,27 @@ input_csv = 'Input_files/spiro_mapi_tio2.csv';
 % par = pc(varargin)
 par = pc(input_csv);
 
+% Other parameters to change:
+% HTL doping level: par.EF0(1)      % (eV)
+% For Ohmic contact- also change Phi_left fot same value
+% ETL doping level: par.EF0(end)    % (eV)
+% For Ohmic contact- also change Phi_right fot same value
+% Active layer thickness: par.d(3)  % (cm)
+% Left-hand workfunction: par.Phi_left      % (eV)
+% Right-hand workfunction: par.Phi_right    % (eV)
+
+% EF0_arr = [-4.6, -4.7, -4.8];
 % Ncat_arr = [1e15, 1e16, 1e17, 1e18, 1e19];
+EF0_arr = -4.7;
 Ncat_arr = [1e15];
 for i = 1:length(Ncat_arr)
     par_struct(i) = par;
     
-    par_struct(i).Ncat = [Ncat_arr(i), Ncat_arr(i), Ncat_arr(i), Ncat_arr(i), Ncat_arr(i)];
+    % par_struct(i).Ncat = [Ncat_arr(i), Ncat_arr(i), Ncat_arr(i), Ncat_arr(i), Ncat_arr(i)];
+    
+    par_struct(i).EF0(1) = EF0_arr(i);
+    par_struct(i).Phi_left = EF0_arr(i);
+    
     % Everytime you change your parameters in a script use this function:
     par_struct(i) = refresh_device(par_struct(i));
     % soleq = equilibrate(varargin)
@@ -65,6 +80,7 @@ for i = 1:length(Ncat_arr)
     sol_IS_OC(i) = IS_script(sol_OC(i), startFreq, endFreq, Freq_points, deltaV, frozen_ions, demodulation, do_graphics);
 end
 
+%% Analysis plots
 % IS_script_exporter(prefix, IS_results)
 IS_script_exporter('unit_testing_deleteme', sol_IS_OC(i))
 
