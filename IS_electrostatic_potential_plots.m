985a7ddbb05296d8331b4faff293c9eec86d59dc
luisina-canto <93191070+luisina-canto@users.noreply.github.com>
2021-12-14 11:51:48 +0000

    Update IS_electrostatic_potential_plots.m

--- IS_electrostatic_potential_plots.m
+++ IS_electrostatic_potential_plots.m
@@ -24,17 +24,19 @@ par = pc(input_csv);
 % Ncat_arr = [1e15, 1e16, 1e17, 1e18, 1e19];
 %EF0_arr = -4.7;
 Ncat_arr = [1e14,1e15, 1e16, 1e17, 1e18, 1e19];
-<<<<<<< Updated upstream
-=======
+
 
 var=Ncat_arr
->>>>>>> Stashed changes
+
 
 %% indentify which parameter we want to vary 
 
 % Calculating the mobility which varies with the reciprocal of ion
 % concentration to give a constant conductivity
-mu_carr = 1e19*1e-10./var;
+mu_carr = 1e19*1e-10./Ncat_arr;
+
+conductivity= mu_carr.*Ncat_arr;
+%%
 
 
 
@@ -76,12 +78,23 @@ for i = 1:length(var)
                
 end
 
+%%
+%% plot energy
+
+for i = 1:length(var)
+                
+                df2plot.ELx(length(var),i,var(i),sol_OC(i));
+             
+
+               
+end
+
 
 %% analyse each zone
 %We want to split it up to the 4 boundaries 
 % we are interested in 3 voltages: initial, middle, final
 
-%need to find V at x1=200nm and x2=600nm
+%need to find V at x1re0nm and x2=600nm
 
 %find voltage at initial parameter:
 
@@ -90,12 +103,9 @@ V_final=[] ;% create a array to store all the initial voltages
 V_boundary1=[]; %this is the potential between the 1st and 2nd boundary
 V_boundary2=[]; %this is the potential between the 3rd and 4th boundary 
 inf_pos=[]; %inflextion position of x array corresponding to find V
-second_dif=[];
-width1=[];
-width2=[]
 
 
-for i = 1:length(Ncat_arr)
+for i = 1:length(var)
     [u,t,x,par,dev,n,p,a,c,V] = dfana.splitsol(sol_OC(i));
     
     V_ini= V(100,1);
@@ -138,11 +148,6 @@ for i = 1:length(Ncat_arr)
     V_boundary1(i)=V(100,index_x1);
     V_boundary2(i)=V(100,index_x2);
 
-
-% 
-     figure (10+i)
-     plot(x(1:740),dif)
-%   
 % 
 %     x_index0=[];
 %     x_index01=[];
@@ -174,50 +179,102 @@ for i = 1:length(Ncat_arr)
 %now we find the actual position of x
 %%
 %first width
-recom_boundaries=[];
-recom_boundaries2=[];
-
-for k=1:length(x_index0)-1
-    if abs(x_index0(k+1)-x_index0(k))>1
-        recom_boundaries=[recom_boundaries,x(k)];
-    end    
-
-end
+% recom_boundaries=[];
+% recom_boundaries2=[];
 % 
-% width_1=recom_boundaries(2)-recom_boundaries(1);
-% width1=[width1, width_1];
-
-for k=380:length(x_index01)-1
-    if abs(x_index01(k+1)-x_index01(k))>1
-        
-        recom_boundaries2=[recom_boundaries2,x(k)];
-    end    
-
-end
+% for k=1:length(x_index0)-1
+%     if abs(x_index0(k+1)-x_index0(k))>1
+%         recom_boundaries=[recom_boundaries,x(k)];
+%     end    
+% 
+% end
+% % 
+% % width_1=recom_boundaries(2)-recom_boundaries(1);
+% % width1=[width1, width_1];
+% 
+% for k=380:length(x_index01)-1
+%     if abs(x_index01(k+1)-x_index01(k))>1
+%         
+%         recom_boundaries2=[recom_boundaries2,x(k)];
+%     end    
+% 
+% end
 % 
 % width_2=recom_boundaries2(2)-recom_boundaries2(1);
 % width2=[width2, width_2];
 
 
 
+%% CALCULATING CAPACITANCE
+%% Plotting Charge density
+
+for i = 1:length(var)
+                
+                df2plot.rhoxVx(length(var),i,var(i),sol_OC(i));
+               
+end
+
+%% CALCULATING CAPCITANCE 
+
+
+% integrate for area of interest
+
+charge_region1=[];
+charge_region2=[];
+
+for i=1:length(var)
+    [xcap,ycap]=rhoxVx(length(var),i,var(i),sol_OC(i));
+    xcap=xcap.*1e-7;
+
+    %we already have the index at the boundaries (200,600nm) from above
+    %index_x1 = index of first boundary x=200nm
+    %index_x2 = index of second boundary x=600nm
+    
+    %integrate from x=0 to x=200 first
+    cap1=trapz(xcap(1:index_x1),ycap(1:index_x1));
+    charge_region1(i)=cap1;
+
+    %integrate from x=600 to x=700(end)
+    cap2=trapz(xcap(index_x2:end),ycap(index_x2:end));
+    charge_region2(i)=cap2;
+
+
+end  
+
+
+
+
+%% CALCULATING RECOMBINATION RESISTANCE 
+%% Calculate recombination currents
+
+recom_cur1=[];
+
+for i=1:length(var)
+    Jcomp=Current_Contributions(sol_OC(i),1);
+    cur1=Jcomp.J_vsr(1,100);
+    cur2=Jcomp.J_vsr(2,100);
+    recom_cur1(i)=cur1;
+    recom_cur2(i)=cur2;
+
+
+   
+
+
+end   
+
+
+
+
+
+
+
+
+
+
+
+
+
 %%
-
-
-%% relative recombionation 
-
-%need relative space charge layer widths
-%the width is the distance between the 2 curves being flat
-%width_r1= width of the 1st region in between green and white 
-%width_r2= width of region between white and blue 
-
-
-
-%%
-
-%recombination rates
-
-%% integrate rx to give tot_r (area under rx graph)
-
 x=[];
 y=[];
 tot_r=[];
@@ -228,36 +285,9 @@ for i=1:length(var)
 
 end   
 
-%% finding recombination for chosen regions
-
-%setting up the values 
-
-Ncat=[0 0 0 0 0 0; 1.68E-05	2.00E-05	4.77E-05	4.77E-05	6.00E-05	6.80E-05
-;1.55E-05	2.00E-05	4.50E-05	4.50E-05	6.00E-05	6.36E-05
-;1.04969E-05	2.00E-05	0.000039768	4.17544E-05	6.00E-05	6.85828E-05
-;0.000010249	2.00E-05	0.000229958	5.75298E-05	6.00E-05	6.39767E-05;...
-1.02E-05	2.00E-05	2.52E-05	0.0000581	6.00E-05	0.0000647];
-
-%Ncat=[{},{1.68E-05,2.00E-05,4.77E-05,4.77E-05,6.00E-05,6.80E-05},{1.55E-05,2.00E-05,4.50E-05,4.50E-05,6.00E-05,6.36E-05},{1.04969E-05,2.00E-05,0.000039768,4.17544E-05,6.00E-05,6.85828E-05}...
-%,{0.000010249,2.00E-05,0.000229958,5.75298E-05,6.00E-05,6.39767E-05},{1.02E-05,2.00E-05,2.52E-05,0.0000581,6.00E-05,0.0000647}];
-
-%%
 
 
 
-for i=1:length(var)
-   [x y]=rx(length(var),i,var(i),sol_OC(i));
-   x1=x(Ncat(i,1):Ncat(i,2));
-   y1=y(Ncat(i,1):Ncat(i,2));
-
-
-end   
-
-
-
- 
-
-%%
 
 
 
@@ -273,7 +303,22 @@ end
             x=x_sub*1e7;
             
            
-        end
+ end
+
+ %%
+
+ function [x,y]=rhoxVx(leng,loop_index,value,varargin)
+            [sol, tarr, pointtype, xrange] = dfplot.sortarg(varargin);
+            [u,t,x,par,dev,n,p,a,c,V] = dfana.splitsol(sol);
+             x_sub = par.x_sub;
+             rho = dfana.calcrho(sol, "whole");
+             y=rho(100,:);
+             x=x*1e7
+            
+             
+ end             
+
+
 
 %% funciton to get the inflection point 
 %for electrostatic graphs  which dont have a clear straight line at the mid
@@ -300,8 +345,4 @@ function dif=differential(x,V)
     dif=dVp./dx;
 
 end
-
-
-
-
 
