89a02bc29a7ca74013222292b68867d1a5e331c1
pdb13 <p.calado13@imperial.ac.uk>
2022-01-05 15:19:56 +0000

    i =6 instead of 5

--- IS_full_method_script_Nani.m
+++ IS_full_method_script_Nani.m
@@ -90,7 +90,7 @@ deltaV = 2e-3;
 frozen_ions = false;
 demodulation = true;
 do_graphics = false;
-parfor i = 5
+parfor i = 6
     i
     % IS_results = IS_script(structs, startFreq, endFreq, Freq_points, deltaV, frozen_ions, demodulation, do_graphics)
     sol_IS_SC(i) = IS_script(soleq(i).ion, startFreq, endFreq, Freq_points, deltaV, frozen_ions, demodulation, do_graphics);
